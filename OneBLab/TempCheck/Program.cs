18ca2a0ad170033e1f05f8b7b62c9727e4bd1831
Aaron Bates <10880292@uvu.edu>
2024-01-31 18:39:27 -0700

    Added Unit 1B Lab Files

---
+++ OneBLab/TempCheck/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the current temperature (in celcius):"); //Displays the phrase in quotation marks on the console
+        int temp = Convert.ToInt32(Console.ReadLine()); //Takes the user's input and converts it into a number that the system can use
+        if (temp >= 30) //Checks if the temperature is equal to or greater to 30 degrees, and runs the following code if so
+        {
+            Console.WriteLine("It's really hot! Be sure to stay hydrated and don't stay in the sun for too long!");
+        }
+        else //This portion of code runs if the temperature is NOT equal to or greater than 30.
+        {
+            Console.WriteLine("Enjoy the pleasant weather!");
+        }
+    }
+}
