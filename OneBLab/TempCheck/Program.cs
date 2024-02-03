4a8729e021886e182db4879211f8404d869e256a
Aaron Bates <10880292@uvu.edu>
2024-02-02 20:56:54 -0700

    Updated my temperature program for the Unit 1b challenge

--- OneBLab/TempCheck/Program.cs
+++ OneBLab/TempCheck/Program.cs
@@ -8,6 +8,14 @@
         {
             Console.WriteLine("It's really hot! Be sure to stay hydrated and don't stay in the sun for too long!");
         }
+        else if (temp <= 10)
+        {
+            Console.WriteLine("It's really cold! Be sure to wear cold clothing!");
+        }
+        else if (temp <= 20)
+        {
+            Console.WriteLine("It's a bit chilly out there. Be sure to wear a light jacket!");
+        }
         else //This portion of code runs if the temperature is NOT equal to or greater than 30.
         {
             Console.WriteLine("Enjoy the pleasant weather!");
