7a0d9ef0ed1a5c9cb041ae20c69f63734374c058
Aaron Bates <10880292@uvu.edu>
2024-04-10 22:24:27 -0600

    Finished Lab 6a

---
+++ UFO Defence Force Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 25;
+    public float xRange = 30;
+    public Transform blaster;
+    public GameObject laserBolt;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Initializes horizontal input
+        horizontalInput = Input.GetAxis("Horizontal"); 
+        // Moves player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // Keep player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        // If space bar is pressed, fire laser bolt
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Create laserBolt at the blaster transform position while maintaining the object's rotation.
+            Instantiate(laserBolt, blaster.transform.position, laserBolt.transform.rotation);
+        }
+    }
+
+    // Delete any object with a trigger that hits the player
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
