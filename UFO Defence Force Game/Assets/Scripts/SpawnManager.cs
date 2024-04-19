c7133080956417b06bcf88b0ed217f0d4067053c
Aaron Bates <10880292@uvu.edu>
2024-04-19 16:36:20 -0600

    Finished Challenge 6b

--- UFO Defence Force Game/Assets/Scripts/SpawnManager.cs
+++ UFO Defence Force Game/Assets/Scripts/SpawnManager.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] ufoPrefabs; // Array to store spawnable objects
+    public GameObject ore; // Stores the collectable object
     private float spawnRangeX = 20.0f;
     private float spawnPosZ = 20.0f;
     private float startDelay = 2.0f;
     private float spawnInterval = 1.5f;
+    public int enemyCount;
+    private float spawnDelay;
     void Start()
     {
+        spawnDelay = Random.Range(startDelay * 3, startDelay * 7);
         InvokeRepeating("SpawnRandomUFO", startDelay, spawnInterval);
+        InvokeRepeating("SpawnCollectable", startDelay, spawnDelay);
     }
     // Update is called once per frame
     void Update()
@@ -24,5 +30,18 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
         int ufoIndex = Random.Range(0, ufoPrefabs.Length); // Picks a random UFO from the array
         Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation); // Spawns indexed UFO from array on a random position on the X axis
+    }
+
+    void SpawnCollectable()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        spawnDelay = Random.Range(startDelay * 3, startDelay * 7);
+        CountDown(spawnDelay);
+        Instantiate(ore, spawnPos, ore.transform.rotation); // Spawns indexed collectable on a random position on the X axis
+    }
+
+    IEnumerator CountDown(float timeMultiplier)
+    {
+        yield return new WaitForSeconds(spawnInterval * timeMultiplier);
     }
 }
